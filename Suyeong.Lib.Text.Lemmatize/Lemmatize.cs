02fb9f258e234260c69414db081bd542425868f1
suyeongpark <suyeongpark@abyne.com>
2019-02-27 15:01:43 +0900

    lemma sharp 추가.

---
+++ Suyeong.Lib.Text.Lemmatize/Lemmatize.cs
@@ -0,0 +1,19 @@
+﻿using LemmaSharp;
+
+namespace Suyeong.Lib.Text.Lemmatize
+{
+    public class Lemmatize
+    {
+        ILemmatizer lmtz;
+
+        public Lemmatize(LanguagePrebuilt language = LanguagePrebuilt.English)
+        {
+            this.lmtz = new LemmatizerPrebuiltCompact(language);
+        }
+
+        public string GetLemmaWord(string lowerWord)
+        {
+            return this.lmtz.Lemmatize(lowerWord);
+        }
+    }
+}
