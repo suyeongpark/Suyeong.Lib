d91d17e01305459111c91f6a23da995ec85c45f6
suyeongpark <suyeongpark@abyne.com>
2019-05-07 17:23:30 +0900

    강제로 돌리는거 부분 합침.

--- Suyeong.Lib.Doc.PdfAcrobat/PdfAcrobat.cs
+++ Suyeong.Lib.Doc.PdfAcrobat/PdfAcrobat.cs
@@ -18,23 +18,33 @@ namespace Suyeong.Lib.Doc.PdfAcrobat
         {
             PdfPages pages = new PdfPages();
 
-            AcroPDDoc acroPdDoc = null;
+            AcroAVDoc acroAvDoc = null;
+            CAcroPDDoc acroPdDoc = null;
 
             try
             {
-                acroPdDoc = Activator.CreateInstance(Type.GetTypeFromProgID(PD_DOC_PROG_ID)) as AcroPDDoc;
+                acroAvDoc = new AcroAVDoc();
+                acroAvDoc.Open(filePath, "");
 
-                if (acroPdDoc.Open(filePath))
+                if (acroAvDoc.IsValid())
                 {
+                    acroPdDoc = acroAvDoc.GetPDDoc() as CAcroPDDoc;
+
                     int pageCount = acroPdDoc.GetNumPages();
 
                     if (pageCount > 0)
                     {
-                        pages = GetPdfPages(pageCount: pageCount, acroPdDoc: acroPdDoc);
+                        pages = GetPdfPages(pageCount: pageCount, filePath: filePath, acroPdDoc: ref acroPdDoc, acroAvDoc: ref acroAvDoc);
                     }
+
+                    acroPdDoc.Close();
+                }
+                else
+                {
+                    throw new InvalidOperationException();
                 }
 
-                acroPdDoc.Close();
+                acroAvDoc.Close(1);
             }
             catch (Exception)
             {
@@ -42,80 +52,18 @@ namespace Suyeong.Lib.Doc.PdfAcrobat
             }
             finally
             {
-                ReleaseComObjects(new object[] { acroPdDoc, });
+                ReleaseComObjects(new object[] { acroAvDoc, acroPdDoc, });
+                GC.Collect();
             }
 
             return pages;
         }
 
-        /// <summary>
-        /// PDF 페이지 수가 많을 때 COM 오브젝트가 뻗는다. 강제로 돌리기 위한 메서드
-        /// </summary>
-        /// <param name="filePath"></param>
-        /// <returns></returns>
-        public static PdfPages GetRawTextForce(string filePath)
-        {
-            PdfPages pages = new PdfPages();
-
-            AcroPDDoc acroPdDoc = null;
-
-            try
-            {
-                acroPdDoc = Activator.CreateInstance(Type.GetTypeFromProgID(PD_DOC_PROG_ID)) as AcroPDDoc;
-
-                if (acroPdDoc.Open(filePath))
-                {
-                    int pageCount = acroPdDoc.GetNumPages();
-
-                    if (pageCount > 0)
-                    {
-                        pages = GetPdfPagesForce(pageCount: pageCount, filePath: filePath, acroPdDoc: acroPdDoc);
-                    }
-                }
-
-                acroPdDoc.Close();
-            }
-            catch (Exception)
-            {
-                // 강제로 할 때는 예외를 throw 하지 않는다.
-            }
-            finally
-            {
-                ReleaseComObjects(new object[] { acroPdDoc, });
-            }
-
-            return pages;
-        }
-
-        static PdfPages GetPdfPages(int pageCount, AcroPDDoc acroPdDoc)
+        static PdfPages GetPdfPages(int pageCount, string filePath, ref CAcroPDDoc acroPdDoc, ref AcroAVDoc acroAvDoc)
         {
             PdfPages pdfPages = new PdfPages();
 
-            AcroPDPage acroPdPage;
-            int width, height, rotate, indexX1, indexX2, indexY1, indexY2;
-            PdfTexts pdfTexts;
-            object jso = acroPdDoc.GetJSObject();
-
-            for (int i = 0; i < pageCount; i++)
-            {
-                acroPdPage = acroPdDoc.AcquirePage(i);
-                width = acroPdPage.GetSize().x;
-                height = acroPdPage.GetSize().y;
-                rotate = acroPdPage.GetRotate();
-                FindPositionIndex(rotate: rotate, indexX1: out indexX1, indexX2: out indexX2, indexY1: out indexY1, indexY2: out indexY2);
-                pdfTexts = GetPdfTexts(pageIndex: i, indexX1: indexX1, indexX2: indexX2, indexY1: indexY1, indexY2: indexY2, jso: jso);
-
-                pdfPages.Add(new PdfPage(index: i, width: width, height: height, rotate: rotate, pdfTexts: pdfTexts));
-            }
-
-            return pdfPages;
-        }
-
-        static PdfPages GetPdfPagesForce(int pageCount, string filePath, AcroPDDoc acroPdDoc)
-        {
-            PdfPages pdfPages = new PdfPages();
-
-            AcroPDPage acroPdPage;
+            CAcroPDPage acroPdPage;
             int width, height, rotate, indexX1, indexX2, indexY1, indexY2;
             PdfTexts pdfTexts;
             object jso = acroPdDoc.GetJSObject();
@@ -134,15 +82,13 @@ namespace Suyeong.Lib.Doc.PdfAcrobat
                 }
                 catch (Exception)
                 {
-                    ReleaseComObjects(new object[] { acroPdDoc, });
-
                     // 페이지가 많으면 중간에 뻗는다. 다시 살리기 위한 코드.
-                    acroPdDoc = new AcroPDDoc();
-                    acroPdDoc = Activator.CreateInstance(Type.GetTypeFromProgID(PD_DOC_PROG_ID)) as AcroPDDoc;
-                    acroPdDoc.Open(filePath);
+                    acroAvDoc = new AcroAVDoc();
+                    acroAvDoc.Open(filePath, "");
+                    acroPdDoc = acroAvDoc.GetPDDoc() as CAcroPDDoc;
                     jso = acroPdDoc.GetJSObject();
 
-                    // 살린 후에 다시 돌린다.
+                    // 살린 후에 해당 페이지를 다시 돌린다.
                     pdfTexts = GetPdfTexts(pageIndex: i, indexX1: indexX1, indexX2: indexX2, indexY1: indexY1, indexY2: indexY2, jso: jso);
                 }
 
@@ -152,76 +98,29 @@ namespace Suyeong.Lib.Doc.PdfAcrobat
             return pdfPages;
         }
 
-        static void FindPositionIndex(int rotate, out int indexX1, out int indexX2, out int indexY1, out int indexY2)
-        {
-            indexX1 = indexX2 = indexY1 = indexY2 = -1;
-
-            // 회전값 0일 때
-            // 0 - LT X
-            // 1 - LT Y
-            // 2 - RT X
-            // 3 - RT Y
-            // 4 - LB X
-            // 5 - LB Y
-            // 6 - RB X
-            // 7 - RB Y
-
-            // 회전값 90 일 때                    
-            // 0 - LT Y
-            // 1 - LT X
-            // 2 - RT Y
-            // 3 - RT X
-            // 4 - LB Y
-            // 5 - LB X
-            // 6 - RB Y
-            // 7 - RB X
-
-            if (rotate == 90)
-            {
-                indexX1 = 1;
-                indexX2 = 7;
-                indexY1 = 6;
-                indexY2 = 0;
-            }
-            else if (rotate == 270 || rotate == -90)
-            {
-                indexX1 = 7;
-                indexX2 = 1;
-                indexY1 = 0;
-                indexY2 = 6;
-            }
-            else
-            {
-                indexX1 = 0;
-                indexX2 = 6;
-                indexY1 = 1;
-                indexY2 = 7;
-            }
-        }
-
         static PdfTexts GetPdfTexts(int pageIndex, int indexX1, int indexX2, int indexY1, int indexY2, object jso)
         {
             PdfTexts pdfTexts = new PdfTexts();
 
+            Type type = jso.GetType();
             object jsNumWords, jsWord;
-            object[] positionArr;
-            dynamic jsQuads;
+            object[] jsQuads, positionArr;
             string text;
             double x1, x2, y1, y2;
             int wordsCount;
 
-            jsNumWords = jso.GetType().InvokeMember(JSO_GET_PAGE_NUM_WORDS, BindingFlags.InvokeMethod, null, jso, new object[] { pageIndex }, null);
+            jsNumWords = type.InvokeMember(JSO_GET_PAGE_NUM_WORDS, BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Instance, null, jso, new object[] { pageIndex }, null);
             wordsCount = int.Parse(jsNumWords.ToString());
 
             for (int i = 0; i < wordsCount; i++)
             {
-                jsWord = jso.GetType().InvokeMember(JSO_GET_PAGE_NTH_WORDS, BindingFlags.InvokeMethod, null, jso, new object[] { pageIndex, i, false }, null);
+                jsWord = type.InvokeMember(JSO_GET_PAGE_NTH_WORDS, BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Instance, null, jso, new object[] { pageIndex, i, false }, null);
                 text = jsWord.ToString().Trim();
 
                 if (!string.IsNullOrWhiteSpace(text))
                 {
-                    jsQuads = jso.GetType().InvokeMember(JSO_GET_PAGE_NTH_WORD_QUADS, BindingFlags.InvokeMethod, null, jso, new object[] { pageIndex, i }, null);
-                    positionArr = jsQuads[0];
+                    jsQuads = type.InvokeMember(JSO_GET_PAGE_NTH_WORD_QUADS, BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Instance, null, jso, new object[] { pageIndex, i }, null) as object[];
+                    positionArr = jsQuads[0] as object[];
 
                     // 텍스트 자체가 회전된 경우가 있는데, 당장은 처리 못 함.
                     x1 = double.Parse(positionArr[indexX1].ToString());
@@ -253,9 +152,8 @@ namespace Suyeong.Lib.Doc.PdfAcrobat
                     current = oldTexts[i];
                     last = newTexts[newTexts.Count - 1];
 
-                    // 같은 줄이고, 간격이 좁으면 합친다.
-                    // 간혹 오른쪽 것이 먼저 나오는 경우가 있어서 좌우 길이 비교할 때 abs를 취한다.
-                    if (Math.Abs(current.BottomY - last.BottomY) < DISTANCE_SHORT && Math.Abs(current.LeftX - last.RightX) < DISTANCE_SHORT)
+                    // 같은 줄이고, 간격이 좁으면
+                    if (Math.Abs(current.BottomY - last.BottomY) < DISTANCE_SHORT && current.LeftX - last.RightX < DISTANCE_SHORT)
                     {
                         // 마지막 것을 덮어쓴다.
                         newTexts[newTexts.Count - 1] = last + current;
@@ -268,6 +166,46 @@ namespace Suyeong.Lib.Doc.PdfAcrobat
             }
 
             return newTexts;
+        }
+
+        static void FindPositionIndex(int rotate, out int indexX1, out int indexX2, out int indexY1, out int indexY2)
+        {
+            indexX1 = indexX2 = indexY1 = indexY2 = -1;
+
+            // 회전값 0일 때
+            // 0 - LT X
+            // 1 - LT Y
+            // 2 - RT X
+            // 3 - RT Y
+            // 4 - LB X
+            // 5 - LB Y
+            // 6 - RB X
+            // 7 - RB Y
+
+            // 회전값 90 일 때                    
+            // 0 - LT Y
+            // 1 - LT X
+            // 2 - RT Y
+            // 3 - RT X
+            // 4 - LB Y
+            // 5 - LB X
+            // 6 - RB Y
+            // 7 - RB X
+
+            if (rotate == 90 || rotate == 270 || rotate == -90)
+            {
+                indexX1 = 1;
+                indexX2 = 7;
+                indexY1 = 0;
+                indexY2 = 6;
+            }
+            else
+            {
+                indexX1 = 0;
+                indexX2 = 6;
+                indexY1 = 1;
+                indexY2 = 7;
+            }
         }
 
         static bool ReleaseComObjects(object[] objects)
