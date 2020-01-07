15ab54f7d5802f0089b26f45739ad0835de14249
suyeongpark <suyeongpark@abyne.com>
2020-01-07 17:05:39 +0900

    문서에서 문장 추출하는 전처리 라이브러리 추가.

---
+++ Suyeong.Lib.Doc.Detector/TextSetDetector.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Suyeong.Lib.Doc.Detector
+{
+    public static class TextSetDetector
+    {
+        const double SIZE_RATIO = 0.5d;
+        const double ALIGN_RATIO = 0.1d;
+        const double SPACING_RATIO = 1.2d;
+
+        /// <summary>
+        /// 컬럼을 나누는 것은 MergeTextLine로 line을 가져온 후에 해야 한다.
+        /// MergeTextsGroup은 도면 같이 아예 줄 같은게 없는 곳에서 뭉쳐진 텍스트틀 합하는 것.
+        /// </summary>
+
+        public static TextBlockSetCollection MergeTextLineByHorizontal(TextBlockCollection sources, double sizeRatio = SIZE_RATIO)
+        {
+            TextBlockSetCollection lineTexts = new TextBlockSetCollection();
+
+            TextBlockCollection textsLine;
+            TextBlock main, sub;
+
+            Dictionary<int, int> duplicateDic = new Dictionary<int, int>();
+            int index = 0;
+
+            for (int i = 0; i < sources.Count; i++)
+            {
+                if (!duplicateDic.ContainsKey(i))
+                {
+                    main = sources[i];
+
+                    textsLine = new TextBlockCollection();
+                    textsLine.Add(main);
+
+                    duplicateDic.Add(i, i);
+
+                    for (int j = i + 1; j < sources.Count; j++)
+                    {
+                        sub = sources[j];
+
+                        if (!duplicateDic.ContainsKey(j))
+                        {
+                            if (Math.Abs(main.Height - sub.Height) < main.Height * sizeRatio &&
+                                main.TopY <= sub.BottomY && main.BottomY >= sub.TopY)
+                            {
+                                textsLine.Add(sub);
+                                duplicateDic.Add(j, j);
+                            }
+                        }
+                    }
+
+                    lineTexts.Add(new TextBlockSet(index: index++, textBlocks: new TextBlockCollection(textsLine.OrderBy(text => text.LeftX))));
+                }
+            }
+
+            return lineTexts;
+        }
+
+        public static TextBlockSetCollection MergeTextLineByVertical(TextBlockCollection sources, bool isBottomFirst = true, double sizeRatio = SIZE_RATIO)
+        {
+            TextBlockSetCollection lineTexts = new TextBlockSetCollection();
+
+            TextBlockCollection textsLine, textsLineOrder;
+            TextBlock main, sub;
+
+            Dictionary<int, int> duplicateDic = new Dictionary<int, int>();
+            int index = 0;
+
+            for (int i = 0; i < sources.Count; i++)
+            {
+                if (!duplicateDic.ContainsKey(i))
+                {
+                    main = sources[i];
+
+                    textsLine = new TextBlockCollection();
+                    textsLine.Add(main);
+
+                    duplicateDic.Add(i, i);
+
+                    for (int j = i + 1; j < sources.Count; j++)
+                    {
+                        sub = sources[j];
+
+                        if (!duplicateDic.ContainsKey(j))
+                        {
+                            if (Math.Abs(main.Width - sub.Width) < main.Width * sizeRatio &&
+                                main.LeftX <= sub.RightX && main.RightX >= sub.LeftX)
+                            {
+                                textsLine.Add(sub);
+                                duplicateDic.Add(j, j);
+                            }
+                        }
+                    }
+
+                    textsLineOrder = isBottomFirst ? new TextBlockCollection(textsLine.OrderBy(text => text.BottomY)) : new TextBlockCollection(textsLine.OrderBy(text => text.TopY));
+                    lineTexts.Add(new TextBlockSet(index: index++, textBlocks: textsLineOrder));
+                }
+            }
+
+            return lineTexts;
+        }
+
+        public static TextBlockSetCollection MergeTextGroupByHorizontal(TextBlockCollection sources, double sizeRatio = SIZE_RATIO, double alignRatio = ALIGN_RATIO, double spacingRatio = SPACING_RATIO)
+        {
+            TextBlockSetCollection groupTexts = new TextBlockSetCollection();
+
+            TextBlockCollection texts;
+
+            Dictionary<int, int> duplicateDic = new Dictionary<int, int>();
+
+            foreach (TextBlock source in sources)
+            {
+                if (!duplicateDic.ContainsKey(source.Index))
+                {
+                    texts = new TextBlockCollection();
+                    texts.Add(source);
+
+                    duplicateDic.Add(source.Index, source.Index);
+
+                    FindHorizontalTextsRecursive(current: source, sizeRatio: sizeRatio, alignRatio: alignRatio, spacingRatio: spacingRatio, texts: ref texts, sources: ref sources, duplicateDic: ref duplicateDic);
+
+                    groupTexts.Add(new TextBlockSet(index: groupTexts.Count, textBlocks: new TextBlockCollection(texts.OrderBy(text => text.LeftX))));
+                }
+            }
+
+            return groupTexts;
+        }
+
+        public static TextBlockSetCollection MergeTextGroupByVertical(TextBlockCollection sources, bool isBottomFirst = true, double sizeRatio = SIZE_RATIO, double alignRatio = ALIGN_RATIO, double spacingRatio = SPACING_RATIO)
+        {
+            TextBlockSetCollection groupTexts = new TextBlockSetCollection();
+
+            TextBlockCollection texts, textsOrder;
+
+            Dictionary<int, int> duplicateDic = new Dictionary<int, int>();
+
+            foreach (TextBlock source in sources)
+            {
+                if (!duplicateDic.ContainsKey(source.Index))
+                {
+                    texts = new TextBlockCollection();
+                    texts.Add(source);
+
+                    duplicateDic.Add(source.Index, source.Index);
+
+                    FindVerticalTextsRecursive(current: source, sizeRatio: sizeRatio, alignRatio: alignRatio, spacingRatio: spacingRatio, texts: ref texts, sources: ref sources, duplicateDic: ref duplicateDic);
+
+                    textsOrder = isBottomFirst ? new TextBlockCollection(texts.OrderBy(text => text.BottomY)) : new TextBlockCollection(texts.OrderBy(text => text.TopY));
+                    groupTexts.Add(new TextBlockSet(index: groupTexts.Count, textBlocks: textsOrder));
+                }
+            }
+
+            return groupTexts;
+        }
+
+        static void FindHorizontalTextsRecursive(TextBlock current, double sizeRatio, double alignRatio, double spacingRatio, ref TextBlockCollection texts, ref TextBlockCollection sources, ref Dictionary<int, int> duplicateDic)
+        {
+            int size = (int)Math.Round(current.Height * sizeRatio, MidpointRounding.AwayFromZero);
+            int align = (int)Math.Round(current.Height * alignRatio, MidpointRounding.AwayFromZero);
+            int spacing = (int)Math.Round(current.Height * spacingRatio, MidpointRounding.AwayFromZero);
+
+            foreach (TextBlock source in sources)
+            {
+                if (!duplicateDic.ContainsKey(source.Index) &&
+                    IsSameFontSizeByHorizontal(blockA: current, blockB: source, size: size) &&
+                    IsAlignByHorizontal(blockA: current, blockB: source, align: align) &&
+                    IsCloseTextByHorizontal(blockA: current, blockB: source, spacing: spacing))
+                {
+                    texts.Add(source);
+                    duplicateDic.Add(source.Index, source.Index);
+
+                    FindHorizontalTextsRecursive(current: source, sizeRatio: sizeRatio, alignRatio: alignRatio, spacingRatio: spacingRatio, texts: ref texts, sources: ref sources, duplicateDic: ref duplicateDic);
+                }
+            }
+        }
+
+        static void FindVerticalTextsRecursive(TextBlock current, double sizeRatio, double alignRatio, double spacingRatio, ref TextBlockCollection texts, ref TextBlockCollection sources, ref Dictionary<int, int> duplicateDic)
+        {
+            int size = (int)Math.Round(current.Width * sizeRatio, MidpointRounding.AwayFromZero);
+            int align = (int)Math.Round(current.Width * alignRatio, MidpointRounding.AwayFromZero);
+            int spacing = (int)Math.Round(current.Width * spacingRatio, MidpointRounding.AwayFromZero);
+
+            foreach (TextBlock source in sources)
+            {
+                if (!duplicateDic.ContainsKey(source.Index) &&
+                    IsSameFontSizeByVertical(blockA: current, blockB: source, size: size) &&
+                    IsAlignByVertical(blockA: current, blockB: source, align: align) &&
+                    IsCloseTextByVertical(blockA: current, blockB: source, spacing: spacing))
+                {
+                    texts.Add(source);
+                    duplicateDic.Add(source.Index, source.Index);
+
+                    FindVerticalTextsRecursive(current: source, sizeRatio: sizeRatio, alignRatio: alignRatio, spacingRatio: spacingRatio, texts: ref texts, sources: ref sources, duplicateDic: ref duplicateDic);
+                }
+            }
+        }
+
+        static bool IsSameFontSizeByHorizontal(TextBlock blockA, TextBlock blockB, int size)
+        {
+            return Math.Abs(blockA.Height - blockB.Height) < size;
+        }
+
+        static bool IsSameFontSizeByVertical(TextBlock blockA, TextBlock blockB, int size)
+        {
+            return Math.Abs(blockA.Width - blockB.Width) < size;
+        }
+
+        static bool IsAlignByHorizontal(TextBlock blockA, TextBlock blockB, int align)
+        {
+            // line을 찾을 때는 current topY < source bottomY && current bottomY > source topY 이면 충분했지만, line이 없는 문서에서는 정렬을 봐야 한다.
+            // 정렬이 되었으면 같은 단어로 본다.
+            return Math.Abs(blockA.TopY - blockB.TopY) < align || Math.Abs(blockA.CenterY - blockB.CenterY) < align || Math.Abs(blockA.BottomY - blockB.BottomY) < align;
+        }
+
+        static bool IsAlignByVertical(TextBlock blockA, TextBlock blockB, int align)
+        {
+            return Math.Abs(blockA.LeftX - blockB.LeftX) < align || Math.Abs(blockA.CenterX - blockB.CenterX) < align || Math.Abs(blockA.RightX - blockB.RightX) < align;
+        }
+
+        static bool IsCloseTextByHorizontal(TextBlock blockA, TextBlock blockB, int spacing)
+        {
+            return Math.Abs(blockA.LeftX - blockB.RightX) < spacing || Math.Abs(blockA.RightX - blockB.LeftX) < spacing;
+        }
+
+        static bool IsCloseTextByVertical(TextBlock blockA, TextBlock blockB, int spacing)
+        {
+            return Math.Abs(blockA.TopY - blockB.BottomY) < spacing || Math.Abs(blockA.BottomY - blockB.TopY) < spacing;
+        }
+    }
+}
---
+++ Suyeong.Lib.Doc.Detector/Type/TextBlock.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Suyeong.Lib.Doc.Detector
+{
+    public struct TextBlock
+    {
+        public TextBlock(int index, int x, int y, int width, int height, int rotate, string text)
+        {
+            this.Index = index;
+            this.LeftX = x;
+            this.RightX = x + width;
+            this.TopY = y;
+            this.BottomY = y + height;
+            this.CenterX = (int)Math.Round((this.LeftX + this.RightX) * 0.5d, MidpointRounding.AwayFromZero);
+            this.CenterY = (int)Math.Round((this.TopY + this.BottomY) * 0.5d, MidpointRounding.AwayFromZero);
+            this.Width = width;
+            this.Height = height;
+            this.Rotate = rotate;
+            this.Text = text;
+        }
+
+        public int Index { get; private set; }
+        public int LeftX { get; private set; }
+        public int RightX { get; private set; }
+        public int TopY { get; private set; }
+        public int BottomY { get; private set; }
+        public int CenterX { get; private set; }
+        public int CenterY { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public int Rotate { get; private set; }
+        public string Text { get; private set; }
+    }
+
+
+    public class TextBlockCollection : List<TextBlock>
+    {
+        public TextBlockCollection()
+        {
+
+        }
+
+        public TextBlockCollection(IEnumerable<TextBlock> blocks) : base()
+        {
+            this.AddRange(blocks);
+        }
+    }
+}
