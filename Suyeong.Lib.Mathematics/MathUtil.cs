76e8961cdcdd16e67a5d7c1c3d13cad6c2e07c19
suyeongpark <suyeongpark@abyne.com>
2020-03-12 19:21:45 +0900

    라인교차 수식 추가.

--- Suyeong.Lib.Mathematics/MathUtil.cs
+++ Suyeong.Lib.Mathematics/MathUtil.cs
@@ -35,6 +35,26 @@ namespace Suyeong.Lib.Mathematics
             return Math.Abs(num) < double.Epsilon;
         }
 
+        public static double RadianToDegree(double radian)
+        {
+            return radian * 180d / Math.PI;
+        }
+
+        public static double DegreeToRadian(double angle)
+        {
+            return angle * Math.PI / 180d;
+        }
+
+        public static double PixelToPoint(double dpi, double pointPerInch, double value)
+        {
+            return value * pointPerInch / dpi;
+        }
+
+        public static double PointToPixel(double dpi, double pointPerInch, double value)
+        {
+            return value * dpi / pointPerInch;
+        }
+
         public static bool IsPointInLine(int lineStartX, int lineStartY, int lineEndX, int lineEndY, int x, int y)
         {
             int vec1X = x - lineStartX;
@@ -139,24 +159,136 @@ namespace Suyeong.Lib.Mathematics
             return IsEqual(dotProduct * dotProduct, normSquare1 * normSquare2);
         }
 
-        public static double RadianToDegree(double radian)
+        public static bool IsCrossLine(int lineAStartX, int lineAStartY, int lineAEndX, int lineAEndY, int lineBStartX, int lineBStartY, int lineBEndX, int lineBEndY)
         {
-            return radian * 180d / Math.PI;
+            int abX = lineAEndX - lineAStartX;
+            int abY = lineAEndY - lineAStartY;
+
+            int acX = lineBStartX - lineAStartX;
+            int acY = lineBStartY - lineAStartY;
+
+            int adX = lineBEndX - lineAStartX;
+            int adY = lineBEndY - lineAStartY;
+
+            int cdX = lineBEndX - lineBStartX;
+            int cdY = lineBEndY - lineBStartY;
+
+            int caX = lineAStartX - lineBStartX;
+            int caY = lineAStartY - lineBStartY;
+
+            int cbX = lineAEndX - lineBStartX;
+            int cbY = lineAEndY - lineBStartY;
+
+            return GetCCW(ax: abX, ay: abY, bx: acX, by: acY, cx: adX, cy: adY) < 0 && GetCCW(ax: cdX, ay: cdY, bx: caX, by: caY, cx: cbX, cy: cbY) < 0;
         }
 
-        public static double DegreeToRadian(double angle)
+        public static bool IsCrossLine(long lineAStartX, long lineAStartY, long lineAEndX, long lineAEndY, long lineBStartX, long lineBStartY, long lineBEndX, long lineBEndY)
         {
-            return angle * Math.PI / 180d;
+            long abX = lineAEndX - lineAStartX;
+            long abY = lineAEndY - lineAStartY;
+
+            long acX = lineBStartX - lineAStartX;
+            long acY = lineBStartY - lineAStartY;
+
+            long adX = lineBEndX - lineAStartX;
+            long adY = lineBEndY - lineAStartY;
+
+            long cdX = lineBEndX - lineBStartX;
+            long cdY = lineBEndY - lineBStartY;
+
+            long caX = lineAStartX - lineBStartX;
+            long caY = lineAStartY - lineBStartY;
+
+            long cbX = lineAEndX - lineBStartX;
+            long cbY = lineAEndY - lineBStartY;
+
+            return GetCCW(ax: abX, ay: abY, bx: acX, by: acY, cx: adX, cy: adY) < 0 && GetCCW(ax: cdX, ay: cdY, bx: caX, by: caY, cx: cbX, cy: cbY) < 0;
         }
 
-        public static double PixelToPoint(double dpi, double pointPerInch, double value)
+        public static bool IsCrossLine(float lineAStartX, float lineAStartY, float lineAEndX, float lineAEndY, float lineBStartX, float lineBStartY, float lineBEndX, float lineBEndY)
         {
-            return value * pointPerInch / dpi;
+            float abX = lineAEndX - lineAStartX;
+            float abY = lineAEndY - lineAStartY;
+
+            float acX = lineBStartX - lineAStartX;
+            float acY = lineBStartY - lineAStartY;
+
+            float adX = lineBEndX - lineAStartX;
+            float adY = lineBEndY - lineAStartY;
+
+            float cdX = lineBEndX - lineBStartX;
+            float cdY = lineBEndY - lineBStartY;
+
+            float caX = lineAStartX - lineBStartX;
+            float caY = lineAStartY - lineBStartY;
+
+            float cbX = lineAEndX - lineBStartX;
+            float cbY = lineAEndY - lineBStartY;
+
+            return GetCCW(ax: abX, ay: abY, bx: acX, by: acY, cx: adX, cy: adY) < 0 && GetCCW(ax: cdX, ay: cdY, bx: caX, by: caY, cx: cbX, cy: cbY) < 0;
         }
 
-        public static double PointToPixel(double dpi, double pointPerInch, double value)
+        public static bool IsCrossLine(double lineAStartX, double lineAStartY, double lineAEndX, double lineAEndY, double lineBStartX, double lineBStartY, double lineBEndX, double lineBEndY)
         {
-            return value * dpi / pointPerInch;
+            double abX = lineAEndX - lineAStartX;
+            double abY = lineAEndY - lineAStartY;
+
+            double acX = lineBStartX - lineAStartX;
+            double acY = lineBStartY - lineAStartY;
+
+            double adX = lineBEndX - lineAStartX;
+            double adY = lineBEndY - lineAStartY;
+
+            double cdX = lineBEndX - lineBStartX;
+            double cdY = lineBEndY - lineBStartY;
+
+            double caX = lineAStartX - lineBStartX;
+            double caY = lineAStartY - lineBStartY;
+
+            double cbX = lineAEndX - lineBStartX;
+            double cbY = lineAEndY - lineBStartY;
+
+            return GetCCW(ax: abX, ay: abY, bx: acX, by: acY, cx: adX, cy: adY) < 0 && GetCCW(ax: cdX, ay: cdY, bx: caX, by: caY, cx: cbX, cy: cbY) < 0;
+        }
+
+        public static int GetCCW(int ax, int ay, int bx, int by)
+        {
+            return ax * by - ay * bx;
+        }
+
+        public static int GetCCW(int ax, int ay, int bx, int by, int cx, int cy)
+        {
+            return ((bx - ax) * (cy - ay)) - ((cx - ax) * (by - ay));
+        }
+
+        public static long GetCCW(long ax, long ay, long bx, long by)
+        {
+            return ax * by - ay * bx;
+        }
+
+        public static long GetCCW(long ax, long ay, long bx, long by, long cx, long cy)
+        {
+            return ((bx - ax) * (cy - ay)) - ((cx - ax) * (by - ay));
+        }
+
+        public static float GetCCW(float ax, float ay, float bx, float by)
+        {
+            return ax * by - ay * bx;
+        }
+
+        public static float GetCCW(float ax, float ay, float bx, float by, float cx, float cy)
+        {
+            return ((bx - ax) * (cy - ay)) - ((cx - ax) * (by - ay));
+        }
+
+        public static double GetCCW(double ax, double ay, double bx, double by)
+        {
+            return ax * by - ay * bx;
+        }
+
+        public static double GetCCW(double ax, double ay, double bx, double by, double cx, double cy)
+        {
+            return ((bx - ax) * (cy - ay)) - ((cx - ax) * (by - ay));
         }
     }
 }
